089eec64c3b0f7536a2caad55737b39152465719
tmdcjf9912 <tmdcjf991203@gmail.com>
2023-06-02 17:44:12 +0900

    prct

--- src/main/java/com/ssc/exam/board/Main.java
+++ src/main/java/com/ssc/exam/board/Main.java
@@ -1,2 +1,24 @@
-package com.ssc.exam.board;public class Main {
+package com.ssc.exam.board;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("==게시판 v 0.1==");
+        System.out.println("==프로그램 시작==");
+        while(true){
+            System.out.printf("입력)");
+            String cmd = sc.nextLine();
+
+            if(cmd.equals("exit")){
+                break;
+            }
+
+            System.out.printf("입력된 값 : %s\n", cmd);
+        }
+        System.out.println("==프로그램 종료==");
+        sc.close();
+    }
 }
